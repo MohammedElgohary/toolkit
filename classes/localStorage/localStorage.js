b07285e9b8a1ab088fca57a0234a97ac2cc6540f
Mohammed Samara El Gohary <51067446+MohammedElgohary@users.noreply.github.com>
2023-12-17 21:07:56 +0200

    Feat: add the  class to deal with localStorage

---
+++ classes/localStorage/localStorage.js
@@ -0,0 +1,22 @@
+/***
+ * localStorage `class`
+ *
+ */
+export default class LocalStorage {
+  constructor() {}
+
+  /***
+   * @param {string} key
+   * @param {any} defaultValue - fullback data
+   */
+  static getItem(key, defaultValue = null) {
+    return localStorage.getItem(key) ?? defaultValue;
+  }
+
+  static setItem(key, value) {
+    localStorage.setItem(
+      key,
+      typeof value === "string" ? value : JSON.stringify(value)
+    );
+  }
+}
